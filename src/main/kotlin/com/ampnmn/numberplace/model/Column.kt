fd5986e612fa90d5072092207a7f14b2d37e1e9c
ampnmn <dev.ampnmn@gmail.com>
2020-05-10 01:24:00 +0900

    Create Row and Column Model

---
+++ src/main/kotlin/com/ampnmn/numberplace/model/Column.kt
@@ -0,0 +1,8 @@
+package com.ampnmn.numberplace.model
+
+/**
+ * 列
+ */
+data class Column(
+        val cells: List<Cell>
+)
---
+++ src/main/kotlin/com/ampnmn/numberplace/model/Row.kt
@@ -0,0 +1,8 @@
+package com.ampnmn.numberplace.model
+
+/**
+ * 行
+ */
+data class Row(
+        val cells: List<Cell>
+)
